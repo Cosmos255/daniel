0f6608fad7aaae8000b9960b946748cde5d4b75a
Cosmos255 <150472944+Cosmos255@users.noreply.github.com>
2023-12-09 17:05:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
   let name ;
   let age ;
-  window.alert("Bien ai venit pe siteul meu");
-  name = window.prompt("Cum te cheama?");
-  console.log(name);
-  age = window.prompt("In ce an teai nascut?");
-  age = 2023 - age;
-  console.log(age);
+  let email;
+  let password;
+  
 
-  document.getElementById("p1").innerHTML = "Salut " + name;
-  document.getElementById("p2").innerHTML = "Ai varsta de " + age ;
 
 
 
@@ -20,11 +15,53 @@
 
    }
 
+
+
    function dark(){
      document.getElementById('text1').style.color = "red";
+
    }
 
+
+
+
    document.getElementById("regbutton").onclick = function(){
+    name = document.getElementById("name").value;
+    age = 2023 - document.getElementById("year").value;
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
     window.location.assign("https://cosmos255.github.io/daniel/");
     console.log("it worked");
+    console.log(name);
+    console.log(age);
+    console.log(email);
+    console.log(password);
+   
    }
+
+   
+
+
+   if(name == null){
+    document.getElementById("p1").innerHTML = "Salut ";
+    console.log("nullname");
+   }
+
+
+
+   if(age == null){
+    document.getElementById("p2").innerHTML = null;
+    console.log("nullage")
+   }
+
+
+
+   if(window.location.href == 'https://cosmos255.github.io/daniel/log_in.html'){
+    window.alert("Bien ai venit pe siteul meu");
+   }
+
+
+
+
+   document.getElementById("p1").innerHTML = "Salut " + name;
+   document.getElementById("p2").innerHTML = "Ai varsta de " + age;
